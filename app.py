d0d04821e2d86c5b553fa1ddbc5fb94c456b2946
Hugo Ruscitti <hugoruscitti@gmail.com>
2013-01-23 16:10:02 -0300

    Quitando mixines para que cada usuario tenga su propio terminal y no sea compartido.

--- app.py
+++ app.py
@@ -1,28 +1,19 @@
 # -*- encoding: utf-8 -*-
-from gevent import monkey; monkey.patch_all()
+from gevent import monkey
+monkey.patch_all()
+
 from flask import Flask, request, render_template
 
 from socketio import socketio_manage
 from socketio.namespace import BaseNamespace
-from socketio.mixins import RoomsMixin, BroadcastMixin
 import envoy
 
-class TerminalNamespace(BaseNamespace, RoomsMixin, BroadcastMixin):
-
-    def on_nickname(self, nickname):
-        self.environ.setdefault('nicknames', []).append(nickname)
-        self.socket.session['nickname'] = nickname
-        self.broadcast_event('announcement', '%s has connected' % nickname)
-        self.broadcast_event('nicknames', self.environ['nicknames'])
-        self.join('main_room')
-
-    def on_user_message(self, msg):
-        self.emit_to_room('main_room', 'msg_to_room', self.socket.session['nickname'], msg)
+class TerminalNamespace(BaseNamespace):
 
     def on_execute_command(self, command):
         print "Ha llegado el comando", command
         r = envoy.run(command)
-        self.broadcast_event('result', r.std_out)
+        self.emit('result', r.std_out)
 
     def recv_message(self, message):
         print "PING!!!", message
