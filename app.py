39b93ef5d1bf1d153f99ce707c320f736ffda1f8
Hugo Ruscitti <hugoruscitti@gmail.com>
2013-01-23 15:38:19 -0300

    Implementando la ejecución de comandos básica (sin contexto, solo comandos sueltos).

--- app.py
+++ app.py
@@ -5,19 +5,24 @@ from flask import Flask, request, render_template
 from socketio import socketio_manage
 from socketio.namespace import BaseNamespace
 from socketio.mixins import RoomsMixin, BroadcastMixin
+import envoy
 
-# The socket.io namespace
-class ChatNamespace(BaseNamespace, RoomsMixin, BroadcastMixin):
+class TerminalNamespace(BaseNamespace, RoomsMixin, BroadcastMixin):
+
     def on_nickname(self, nickname):
         self.environ.setdefault('nicknames', []).append(nickname)
         self.socket.session['nickname'] = nickname
         self.broadcast_event('announcement', '%s has connected' % nickname)
         self.broadcast_event('nicknames', self.environ['nicknames'])
-        # Just have them join a default-named room
         self.join('main_room')
 
     def on_user_message(self, msg):
         self.emit_to_room('main_room', 'msg_to_room', self.socket.session['nickname'], msg)
+
+    def on_execute_command(self, command):
+        print "Ha llegado el comando", command
+        r = envoy.run(command)
+        self.broadcast_event('result', r.std_out)
 
     def recv_message(self, message):
         print "PING!!!", message
@@ -27,12 +32,11 @@ class ChatNamespace(BaseNamespace, RoomsMixin, BroadcastMixin):
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return render_template('chat.html')
+    return render_template('terminal.html')
 
 @app.route("/socket.io/<path:path>")
 def run_socketio(path):
-    socketio_manage(request.environ, {'': ChatNamespace})
-
+    socketio_manage(request.environ, {'': TerminalNamespace})
 
 if __name__ == '__main__':
     print "Este archivo solo contiene el objeto de aplicación"
