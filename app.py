4d03f006e6311f9244054ca35344d4d0f1d4e31d
Hugo Ruscitti <hugoruscitti@gmail.com>
2013-01-29 01:43:04 -0300

    Mostrando mensajes cuando se publican archivos.

--- app.py
+++ app.py
@@ -14,8 +14,10 @@ from socketio.mixins import BroadcastMixin
 class EventosNamespace(BaseNamespace, BroadcastMixin):
 
     def on_enviar_mensaje(self, usuario, mensaje):
-        print "Ha llegado el mensaje", mensaje
         self.broadcast_event('conversacion', usuario, mensaje)
+
+    def on_publicar(self, usuario, archivo):
+        self.broadcast_event('publicacion', usuario, archivo)
 
 # Flask routes
 app = Flask(__name__)
